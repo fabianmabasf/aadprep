37b4e17c359cbfba84d601ea7a1bb3f72d0e0609
cadullms <cadullms@github.com>
2020-11-16 23:57:20 +0100

    adding stage label

--- theapp/Controllers/HomeController.cs
+++ theapp/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using theapp.Models;
 
@@ -11,15 +12,20 @@ namespace theapp.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly IConfiguration _configuration;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            var stage = _configuration.GetValue<string>("STAGE", "N/A");
+            ViewData["Message"] = $"We are in stage {stage}.";
             return View();
         }
 
